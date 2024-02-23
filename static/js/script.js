21eb276fcf52bedfc11b02751dfe54461a9d3eaf
pha12thom <geofreymilugo@gmail.com>
2024-02-23 15:53:25 +0300

    push static changes managements

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("active");
-}
-
-function closeMenu() {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.remove("active");
-}
