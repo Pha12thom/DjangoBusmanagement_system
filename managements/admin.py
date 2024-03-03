100cc2e3cd82c78bd327018c8b129f3520ead850
pha12thom <MilugoGeofrey.Ouma@studentambassadors.com>
2024-03-03 15:56:09 +0300

    commit admin

--- managements/admin.py
+++ managements/admin.py
@@ -5,7 +5,19 @@ class BusDetails(admin.ModelAdmin):
     def details():
         bus_display = {"name", "no_plate", "departure_city", "departure_time", "seats"}
         return bus_display
-    
+class RouteDetails(admin.ModelAdmin):
+    def details():
+        route_display = {"departure_city", "arrival_city", "distance", "duration"}
+        return route_display
+class ScheduleDetails(admin.ModelAdmin):
+    def details():
+        schedule_display = {"bus", "departTime", "arrivalTime", "price", "route"}
+        return schedule_display
+class BookingDetails(admin.ModelAdmin):
+    def details():
+        booking_display = {"ticket", "seat_no", "passenger_name", "passenger"}
+        
+        
 admin.site.register(Testing)
 admin.site.register(Bus, BusDetails)
 admin.site.register(Route)
