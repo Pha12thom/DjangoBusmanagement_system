4ea3d697a5a33887b7d934a542b7262373c66450
pha12thom <MilugoGeofrey.Ouma@studentambassadors.com>
2024-03-12 20:02:21 +0300

    update admin

--- managements/admin.py
+++ managements/admin.py
@@ -1,29 +1,31 @@
 from django.contrib import admin
-from .models import Bus, Route, Schedule, Ticket, Payment, UserProfile, Testing, Booking
+from .models import Bus, Route, Schedule, Ticket, Payment, UserProfile, Testing
 
 class BusDetails(admin.ModelAdmin):
     def details():
         bus_display = {"name", "no_plate", "departure_city", "departure_time", "seats"}
         return bus_display
-class RouteDetails(admin.ModelAdmin):
-    def details():
-        route_display = {"departure_city", "arrival_city", "distance", "duration"}
-        return route_display
+    
+    
 class ScheduleDetails(admin.ModelAdmin):
-    def details():
-        schedule_display = {"bus", "departTime", "arrivalTime", "price", "route"}
-        return schedule_display
-class BookingDetails(admin.ModelAdmin):
-    def details():
-        booking_display = {"ticket", "seat_no", "passenger_name", "passenger"}
-        
-        
+    def detail():
+        schedule = {
+            "bus", "departTime", "arrivalTime","route"
+        }
+        return schedule
+class RouteDetails(admin.ModelAdmin):
+    def routed():
+        routes = {
+            "departure_city", "arrival_city"," distance","timeTaken", "buses"
+        }  
+        return routes   
+    
+    
 admin.site.register(Testing)
 admin.site.register(Bus, BusDetails)
-admin.site.register(Route)
-admin.site.register(Schedule)
+admin.site.register(Route, RouteDetails)
+admin.site.register(Schedule, ScheduleDetails)
 admin.site.register(Ticket)
 admin.site.register(Payment)
 admin.site.register(UserProfile)
-admin.site.register(Booking)
 
