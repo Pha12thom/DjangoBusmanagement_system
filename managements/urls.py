f2842e04e2e5da175e90982a43c37fd8ea4f2dba
pha12thom <MilugoGeofrey.Ouma@studentambassadors.com>
2024-03-13 19:59:12 +0300

    booking url

--- managements/urls.py
+++ managements/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     path('search_routes/', views.search_routes, name='search_routes'),
     path('search_form/', views.search_form, name='search_form'),
     path('booking/', views.booking, name='booking'),
+    
     path('ticket/<int:ticket_id>/', views.print_ticket, name='print_ticket'),
+    path('print-ticket/', views.print_ticket, name='print_ticket'),
+    path('save_ticket/', views.save_ticket, name='save_ticket'),
+    path('book-ticket/<int:schedule_id>/', views.book_ticket, name='book_ticket'),
+    path('ticket_confirmation/', views.ticket_confirmation, name='ticket_confirmation'),
 
 ]
