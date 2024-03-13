16f2cd0b769b00176e3925038ad538f7da8d95f6
pha12thom <MilugoGeofrey.Ouma@studentambassadors.com>
2024-03-13 19:59:32 +0300

    add booking views

--- managements/views.py
+++ managements/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import RegistrationForm, LoginForm,  SearchForm, BookingForm
-
+from .forms import RegistrationForm, LoginForm,  SearchForm, BookingForm, TicketForm, PrintTicketForm
 from django.contrib.auth.models import User
 from .models import Route, Bus, UserProfile,  Schedule, Ticket
 
@@ -94,6 +93,94 @@ def booking(request, id):
         form = BookingForm()
     return render(request, 'booking_form.html', {'form': form, 'schedule': schedule})
 
-def print_ticket(request, ticket_id):
-    ticket = Ticket.objects.get(id=ticket_id)
-    return render(request, 'ticket.html', {'ticket': ticket})
+
+
+def print_ticket(request):
+    if request.method == 'POST':
+        form = PrintTicketForm(request.POST)
+        if form.is_valid():
+            ticket_id = form.cleaned_data['ticket_id']
+            try:
+                ticket = Ticket.objects.get(id=ticket_id)
+                return render(request, 'print_ticket.html', {'ticket': ticket})
+            except Ticket.DoesNotExist:
+                return HttpResponse('Ticket not found')
+    else:
+        form = PrintTicketForm()
+    return render(request, 'print_ticket_form.html', {'form': form})
+
+
+
+
+def book_ticket(request, schedule_id):
+    schedule = Schedule.objects.get(id=schedule_id)
+    if request.method == 'POST':
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            passenger_name = form.cleaned_data['passenger_name']
+            passenger_age = form.cleaned_data['passenger_age']
+            gender = form.cleaned_data['gender']
+            payment_method = form.cleaned_data['payment_method']
+
+            ticket = Ticket.objects.create(
+                schedule=schedule,
+                passenger_name=passenger_name,
+                passengerAge=passenger_age,
+                gender=gender,
+                user=request.user,
+                pay=payment_method
+            )
+
+            # Assuming payment processing here, create a payment object
+            # payment = Payment.objects.create(ticket=ticket, ...)
+
+            # Redirect to a page where the user can print their ticket
+            return redirect('print_ticket', ticket_id=ticket.id)
+    else:
+        form = TicketForm()
+    return render(request, 'book_ticket.html', {'form': form, 'schedule': schedule})
+
+
+def save_ticket(request):
+    if request.method == 'POST':
+        schedule_id = request.POST.get('schedule')
+        seat_no = request.POST.get('seat_no')
+        passenger_name = request.POST.get('passenger_name')
+        passenger_age = request.POST.get('passengerAge')
+        gender = request.POST.get('gender')
+        pay = request.POST.get('pay')
+        
+        schedule = Schedule.objects.get(id=schedule_id)
+         # Assuming you have authentication enabled
+
+        # Create a new Ticket object and save it to the database
+        ticket = Ticket.objects.create(
+            schedule=schedule,
+            seat_no=seat_no,
+            passenger_name=passenger_name,
+            passengerAge=passenger_age,
+            gender=gender,
+            pay=pay
+        )
+        
+        # Redirect the user to the ticket confirmation page
+        return redirect('ticket_confirmation', ticket_id=ticket.id)
+
+    # Fetch schedules to display in the form
+    schedules = Schedule.objects.all()
+    pays = Ticket.objects.all()
+    return render(request, 'ticket_form.html', {'schedules': schedules, 'pays': pays})
+
+
+
+
+def ticket_confirmation(request):
+    # Fetch the last ticket added to the database
+    last_ticket = Ticket.objects.order_by('-id').first()
+    
+    if last_ticket:
+        return render(request, 'ticket_confirmation.html', {'ticket': last_ticket})
+    else:
+        # Handle case where there are no tickets in the database
+        return render(request, 'ticket_confirmation.html', {'ticket': None})
+
